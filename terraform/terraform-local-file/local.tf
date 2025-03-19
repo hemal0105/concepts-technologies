ad43732c83930b1e884d4bad6b6f8d313b23756b
hemal0105 <hemal0105@gmail.com>
2025-03-20 00:06:22 +0530

    terraform concepts

--- terraform/terraform-local-file/local.tf
+++
@@ -1,4 +0,0 @@
-resource "local_file" "pet" {
-  filename = "data/pets.txt"
-  content = "We love pets!"
-}
---
+++ terraform/terraform-local-file/variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+  default = "data/pets.txt"
+}
+
+variable "content" {
+  default = "We love pets!"
+}
+
+variable "prefix" {
+  default = "Mr"
+}
+
+variable "separator" {
+  default = "."
+}
+
+variable "length" {
+  default = 1
+}
